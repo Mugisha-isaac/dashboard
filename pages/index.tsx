fd5946ea4b4b1f380af3e4bb736c7acd9bafc386
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-01-14 12:14:52 +0200

    added icons on the sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {AiFillSignal,AiOutlinePieChart,AiTwotonePicture,AiTwotoneSetting,AiOutlinePoweroff} from 'react-icons/ai'
 
 export default function Home() {
   return (
     <div className='bg-primary m-3 rounded-2xl ' style={{width:'800px', height:'500px'}}>
      <div>
-       <div className='bg-red-400 w-12 h-40 relative top-7 left-3 rounded-sm'>
-         icons
+       <div className='w-12 h-40 relative top-7 left-3 rounded-sm text-white'>
+         <AiFillSignal className="w-8 h-6" />
+         <AiOutlinePieChart className='w-8 h-6 mt-8'/>
+         <AiTwotonePicture className='w-8 h-6 mt-8'/>
+         <AiTwotoneSetting className='w-8 h-6 mt-8' />
+         <AiOutlinePoweroff className='w-8 h-6' style={{marginTop:'80px'}} />
        </div>
        <span className='text-white text-lg absolute top-7 left-10 px-10'>cards analytics</span>
      </div>
