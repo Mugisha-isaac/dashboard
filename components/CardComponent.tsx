3b6a52eb0a7ea4e1cd618f603c582eb24d204679
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-01-17 10:39:58 +0200

    adding circle icons

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -2,15 +2,26 @@ import { height } from "@mui/system";
 import { FunctionComponent, useState } from "react"
 import { ICard, ITransactions } from "../types"
 import {BsFillPlusCircleFill} from 'react-icons/bs'
+import {BiCircle} from 'react-icons/bi'
 
 
 const CardComponent:FunctionComponent<
 {card:ICard,cardData:Array<ITransactions>}
-> = ({card:{card_name,exp_date,cvv,color,card_number}}) => {
-    let background_color = color;
+> = (
+    {card:{card_name,exp_date,cvv,color,card_number},cardData}) => {
+
+
+    const findLastDate = () =>{
+        for(let i=0;i<cardData.length;i++){
+            if(cardData[i].date > cardData[i+1].date){
+                return cardData[i].date;
+            }
+        }
+    }
+
     return (
         <>
-        <div style={{backgroundColor: background_color, width:'300px', height:'200px'}} className="rounded-2xl relative">
+        <div style={{backgroundColor: color, width:'300px', height:'200px'}} className="rounded-2xl relative">
             <h6 className="uppercase text-sm pt-3 pl-3 text-small_white ">card name</h6> 
             <span className="px-3 bold text-big_white ">{card_name}</span>  
             <div className="absolute top-5" style={{right:'80px'}}>
@@ -33,15 +44,40 @@ const CardComponent:FunctionComponent<
             </div>
         </div>
         <div className="bg-low_dark rounded-2xl relative " style={{width:'480px', marginTop:'-240px', marginLeft:'430px', height:'540px'}} >
-               <div className="border-dotted border-white bg-red-400 absolute top-5 right-5 " style={{width:'120px', height:'140px'}}>
+               <div className="border-dotted border-white bg-white absolute top-5 right-5 " style={{width:'120px', height:'140px'}}>
                  <BsFillPlusCircleFill className="text-white h-7 w-7 bg-none" style={{marginTop:'20px', marginLeft:'50px'}} />
                  <p className="text-small text-white pt-10 pl-5 ">ADD CARD</p>
                </div>
                <div className=" absolute" style={{marginTop:'180px', width:'100%'}}>
                    <p className="capitalize text-xl  text-white px-8">last transactions</p>
-                   <p className="px-8 text-small_white">date</p>
-                   <div>
-                      {/* {trans_name} */}
+                   <p className="px-8 text-small_white">
+                    {
+                        cardData[0].date
+                    }   
+                    </p>
+                   <div className="mt-10">
+                     {
+                         cardData.map(data=>(
+                         <div className=" px-5 mt-3 text-white">
+                             <div className="grid grid-cols-3 grid-rows-1 ml-5 ">
+                                <p>{data.trans_name}</p>
+                                {/* <p className="text-small_white  px-6">{data.status}</p> */}
+                                <div className="px-8">
+                                {
+                                    data.status === 'credit' ?(
+                                        <p className="text-green-400">{data.status}</p>
+                                    ): <p className="text-yellow-400">
+                                        <BiCircle/>
+                                        {data.status}
+                                        </p>
+                                }
+                                </div>
+                                <p>{data.value}</p>
+                                 </div>
+                               
+                             </div>
+                         ))
+                     }
                    </div>
                </div>
             </div>
